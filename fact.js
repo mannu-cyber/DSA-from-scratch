24f058384e6b0bf2054b7f515b80c88b679ad75b
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-20 19:30:59 +0530

    learned factorial

---
+++ fact.js
@@ -0,0 +1,46 @@
+// function factorial(n) {
+//   if (typeof n !== "number" || Math.floor(n) !== n || n < 0) {
+//     return "please enter the valid number";
+//   }
+
+//   if (n === 0) {
+//     return 1;
+//   }
+
+//   return n * factorial(n - 1);
+// }
+
+// console.log(factorial(5));
+
+// function factorial (n) {
+//     if (typeof n!=='number' || Math.floor(n)!==n || n<0){
+//         return('please enter the valid value')
+//     }
+
+// if(n === 0){
+//     return 1
+// }
+
+// return n*factorial(n-1)
+
+// }
+
+// console.log(factorial(7));
+
+// function fact(n) {
+//   if (typeof n !== "number" || n < 0 || Math.floor(n) !== n) {
+//     return "enter the valid value";
+//   }
+
+//   if (n === 0) {
+//     return 1;
+//   }
+
+//   return n * fact(n - 1);
+// }
+
+// console.log(fact(1));
+
+
+
+// findMax([3, 7, 1, 9, 5]);
---
+++ jadu.js
@@ -0,0 +1,24 @@
+// function greet (name){
+//     console.log('good morning ' + name);
+// }
+
+// greet('john')
+
+
+// function jj(name){
+
+// let num = name  - name
+
+// console.log(name);
+//  return num
+// }
+// const  resul  = jj(3,)
+
+// console.log(resul);
+
+
+
+
+function spliteReverse (promopt("enter "))
+
+
