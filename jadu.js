6c55c4d6bae1ab33d5d2ee5772fb1c5b8f32b457
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-22 09:23:12 +0530

    nodeJs run time check

--- jadu.js
+++ jadu.js
@@ -1,24 +1,27 @@
-// function greet (name){
-//     console.log('good morning ' + name);
-// }
+// // function greet (name){
+// //     console.log('good morning ' + name);
+// // }
 
-// greet('john')
-
-
-// function jj(name){
-
-// let num = name  - name
-
-// console.log(name);
-//  return num
-// }
-// const  resul  = jj(3,)
-
-// console.log(resul);
+// // greet('john')
 
 
+// // function jj(name){
+
+// // let num = name  - name
+
+// // console.log(name);
+// //  return num
+// // }
+// // const  resul  = jj(3,)
+
+// // console.log(resul);
 
 
-function spliteReverse (promopt("enter "))
 
 
+// function spliteReverse (promopt("enter "))
+
+
+for (let i = 0; i < 88888; i++) {
+    console.log('😘');
+}
---
+++ question.js
@@ -0,0 +1,2 @@
+var a = 45
+console.log(a);
