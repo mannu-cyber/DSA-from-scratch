147fe08122df3f52fc1c6c10a8afd79d8c3df4ec
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-23 19:31:44 +0530

    add and learn arrow

--- question.js
+++ question.js
@@ -1,2 +1,10 @@
-var a = 45
-console.log(a);
+// arrow function => arrow function created in ES6 version of javascript update and now it is become very popular as compared to the traditional function.
+
+// The syntax is very shorter as compare to tradional function.
+// it makes the code more cleaner and readable.
+// When arrow function has sigle expression we can directly return the value instead of " return or curlybraces11"
+
+
+
+// syntax => 'name of the function' = () =>{}
+
