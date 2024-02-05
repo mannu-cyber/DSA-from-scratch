ef5c8e6312eeb9baf7a2fae7c653d3cffe43e5ee
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-02-05 20:28:24 +0530

    re

---
+++ string.js
@@ -0,0 +1,24 @@
+const mystring = "orange";
+// console.log(mystring.toUpperCase());      // This method is use to convert the string into uppercase.
+
+
+
+// console.log(mystring.indexOf("a"));        // This method is use to find the index number of the variable.
+
+// console.log(mystring.charAt(3));             //  This method is use to find the character position.
+
+
+
+
+// console.log(mystring);
+
+// const mystring2 = new String('this is the string with new string () method')
+// // console.log(typeof mystring2);
+
+
+// const mystringUppercase = mystring2.toUpperCase;
+// console.log(mystringUppercase);
+
+// const streeng = 'newstring';
+
+// console.log(streeng.charAt());
