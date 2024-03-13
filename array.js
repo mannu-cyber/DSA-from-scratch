8c324e3566edba1b4a7cf7c5ff69594aa0922528
cyber-mannu <sahanimannu844@gmail.com>
2024-03-13 11:54:44 +0530

    learned the string and array method

---
+++ array.js
@@ -0,0 +1,135 @@
+// const array = ["banana ", "mango", "apple ", "grapes"];
+
+// array.forEach((currVal, index, array) => {
+//   console.log(
+//     `my favourite frouit is   ${currVal} it is on the ${index} place from the whole ${array}`
+//   );
+// });
+
+// const myMapfun = array.map((curVal, index, Array) => {
+
+//     return `my favourite frouite is ${curVal} it is on the ${index}place from the group of ${Array}`
+
+// });
+
+// console.log(myMapfun);
+
+// const array = ["banana ", "mango", "apple ", "grapes"];
+
+// console.log( array. indexOf('mango', "banana"));
+
+// console.log('the index of the apple is'+ array.indexOf("apple", 1));
+
+// const number = [1, 3, 4, 5, 6, 6];
+
+// console.log(number.indexOf(5));
+
+// console.log(array.indexOf("apple"));
+
+// console.log("Index of 'apple': " + array.map(e => e.trim()).indexOf("apple"));
+
+// console.log('index of apple is ' + array.map( e =>e.trim() (array.indexOf('apple'))));
+
+// console.log(
+//   "the value of apple is" + array.map((e ) => e.trim()).indexOf("apple")
+// );
+
+// const monthsIs = array.indexOf('mango')
+
+// array.splice('banana', 0, 'Banana', 'grapessssss')
+
+// console.log(array);
+// console.log(monthsIs);
+
+// const check = prompt('enter the valu');
+
+// const array = ["banana", "mango", "apple ", "grapes"];
+
+// const updatedVal = array.filter((curval)=>{
+
+//        return curval == check
+// })
+
+// console.log(updatedVal);
+
+// const checkedVal = 500;
+
+// const product = [
+//   {
+//     item: "laptop",
+//     price: 1000,
+//   },
+
+//   {
+//     item: "mobile",
+//     price: 500,
+//   },
+//   {
+//     item: "tablete",
+//     price: 300,
+//   },
+// ];
+
+// const checke = product.filter((checked) => {
+//   return checked.price <= checkedVal;
+// });
+
+// console.log(checke);
+
+// ***************************** here converted the data into the assending order **********************************
+
+// const returnedNumber = number.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+// console.log(returnedNumber);
+
+// ***************************** here converted the data into the decending order **********************************
+
+// const desendingOrder = number.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+
+// console.log(desendingOrder);
+
+// const sqrtNum = number.map((cuVal)={
+
+// if(cuVal % 2 ===0) {
+// return cuVal* cuVal
+// }
+
+// })
+
+// const sqrtNumWithEven = number
+//   .map((curVal) => {
+//     if (curVal % 2 === 0) {
+//       return curVal * curVal;
+//     }
+//   })
+//   .filter((curVal) => curVal !== undefined);
+
+// console.log(sqrtNumWithEven);
+
+// const mulEvenNum = number
+//   .map((curVal) => {
+//     if (curVal % 2 === 0) {
+//       return curVal * curVal;
+//     }
+//   })
+//   .filter((inVal) => inVal !== undefined);
+
+// console.log(mulEvenNum);
+
+const number = [1, 3, 45, 56, 67, 87, 8, 7, 6, 54];
+
+// const number = [];
+
+// ************** when we use reduce method on an array it reduce the all the data in a single data and if we don't provide the initial value in it during the empty array the programme will throw error ************************//
+
+
+// const totalNum = number.reduce((accumilater, curval) => {
+//   return accumilater + curval;
+// },0);
+
+console.log(totalNum);
--- string.js
+++ string.js
@@ -1,20 +1,14 @@
-const mystring = "orange";
+// const mystring = "orange";
 // console.log(mystring.toUpperCase());      // This method is use to convert the string into uppercase.
-
-
 
 // console.log(mystring.indexOf("a"));        // This method is use to find the index number of the variable.
 
 // console.log(mystring.charAt(3));             //  This method is use to find the character position.
 
-
-
-
 // console.log(mystring);
 
 // const mystring2 = new String('this is the string with new string () method')
 // // console.log(typeof mystring2);
-
 
 // const mystringUppercase = mystring2.toUpperCase;
 // console.log(mystringUppercase);
@@ -22,3 +16,49 @@ const mystring = "orange";
 // const streeng = 'newstring';
 
 // console.log(streeng.charAt());
+
+// const result = texts.match('strings')
+// console.log(result);
+
+// const result = texts.forEach((element) => {
+//   element.trim(" ");
+// });
+
+// const result = texts.forEach((elem)=>
+// {
+//        elem
+// })
+
+let texts = "    this is the strings formate   it";
+const trimedWhiteSpace = texts.trim(" ");
+
+// const toArray = trimedWhiteSpace.split(" ").map((curval, index) => {
+//   return `${curval} index ${index}`;
+// });
+
+// console.log(toArray);
+
+console.log(trimedWhiteSpace.slice(1)); // slice() method is use to get the substring from the  string and it accept starting and ending index as an argument and the starting index is included and ending index is excluded
+
+// const toArray = texts.split(' ')  /// this method is use to convert the strings into array  .split()
+
+// const resutl =  toArray.map((curval)=>curval.trim(" ")) // .trim() method is used to remove the extra spaces from the start and ending of the string
+// resutl
+
+// console.log(resutl[4]);
+
+// console.log(resutl);
+// console.log(resutl.includes('this'));
+// console.log(resutl[2]);
+
+// const removeWhiteSpace = Map.texts((curval)=>{
+//        curval.trim("")
+// })
+// const toArray =texts.toArray()
+//  toArray.map(curaval => curaval.trim(' '));
+
+// const toArray = texts.toArray(); // Assuming toArray is a method that converts 'texts' to an array
+// const remove = toArray.map(curaval => curaval.trim());
+
+//  texts.startsWith('this')
+// console.log(result);
