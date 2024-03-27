bfc287622942974d3611e0bef0b008748e98ab39
cyber-mannu <sahanimannu844@gmail.com>
2024-03-27 10:05:43 +0530

    learn time event

--- timeEvent.js
+++ timeEvent.js
@@ -2,6 +2,7 @@ const number = () => {
   console.log("this massage called after every 5 second");
 };
 
-const intervalId = setInterval(number, 1000);
+const intervalId = setInterval(number, 1000); // setInterval is function who excute a block of code at spacific time repeatedly .
 
-setTimeout(() => clearInterval(intervalId), 5000);
+
+setTimeout(() => clearInterval(intervalId), 5000); // settimeOut is a function who excute the a block of code at a spacific time at once 
